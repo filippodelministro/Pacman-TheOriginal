0d673d4efdda17a21847dcde56ec47e800fe88f7
Filippo <pippodelministro@gmail.com>
2023-03-19 12:42:39 +0100

    add image on home [n-w]

--- js/theoriginal.js
+++ js/theoriginal.js
@@ -1,3 +1,10 @@
-function prova(){
-    <p>diocane</p>
-}
+
+function imageAppear() {
+    var x = document.getElementById("menuImage");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
+
