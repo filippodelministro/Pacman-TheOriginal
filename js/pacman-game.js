2caefee02732a551be81c52fad1733a8a571d4c0
Filippo <pippodelministro@gmail.com>
2023-03-19 19:16:38 +0100

    fix moves

--- js/pacman-game.js
+++ js/pacman-game.js
@@ -4,39 +4,47 @@ var level = 0;
 function start(level){
     playground = document.getElementById("playground");
     playground.style.cursor = "none";
-    game = new Game(level);
+    pacmanInit()
+    // game = new Game(level);
 }
 
-function Game(level){
-    document.addEventListener("keyup", this.key)
+// function Game(level){
+//     document.addEventListener("keyup", this.key)
+// }
+
+
+function pacmanInit() {
+    pm = document.getElementById("pacman");
+    pm.style.position = "relative";
+    pm.style.left = "0px";
+    pm.style.top = "0px";
 }
 
-
-
-
-document.onkeydown = detectKey;
-
-function detectKey(e) {
-
-    var posLeft = document.getElementById('pacman').offsetLeft;
-    var posTop = document.getElementById('pacman').offsetTop;
-
-    // e = e || window.event;
-
-    //todo: change in switch
-    if (e.keyCode == '38') {
-        document.getElementById('pacman').style.marginTop  = (posTop-1)+"px";
-    }
-    else if (e.keyCode == '40') {
-        document.getElementById('pacman').style.marginTop  = (posTop+1)+"px";
-    }
-    else if (e.keyCode == '37') {
-        document.getElementById('pacman').style.marginLeft  = (posLeft-1)+"px";
-    }
-    else if (e.keyCode == '39') {
-        document.getElementById('pacman').style.marginLeft  = (posLeft+1)+"px";
+function getKeyAndMove(e) {
+    var key_code = e.which || e.keyCode;
+    switch (key_code) {
+        case 37: moveLeft(); break;
+        case 38: moveUp(); break;
+        case 39: moveRight(); break;
+        case 40: moveDown(); break;
     }
 }
 
 
+//todo: capire ParseInt
+function moveLeft() {
+    pm.style.left = parseInt(pm.style.left) - 5 + "px";
+}
+function moveUp() {
+    pm.style.top = parseInt(pm.style.top) - 5 + "px";
+}
+function moveRight() {
+    pm.style.left = parseInt(pm.style.left) + 5 + "px";
+}
+function moveDown() {
+    pm.style.top = parseInt(pm.style.top) + 5 + "px";
+}
+
+
+window.onload = start;
 
