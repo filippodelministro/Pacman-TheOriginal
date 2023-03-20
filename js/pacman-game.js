b9e5cb5b8e57ef9506e0909da7eb8638b8c77f52
Filippo <pippodelministro@gmail.com>
2023-03-20 13:01:47 +0100

    start, pause & move [n-w]

--- js/pacman-game.js
+++ js/pacman-game.js
@@ -20,56 +20,79 @@ function pacmanInit() {
     pm.style.top = "0px";
 }
 
-function getKeyAndMove(e) {
-    var key_code = e.which || e.keyCode;
-    switch (key_code) {
-        case 37: moveLeft(); break;
-        case 38: moveUp(); break;
-        case 39: moveRight(); break;
-        case 40: moveDown(); break;
-    }
-}
-
-
-//todo: capire ParseInt
-function moveLeft() {
-    pm.style.left = parseInt(pm.style.left) - 5 + "px";
-}
-function moveUp() {
-    pm.style.top = parseInt(pm.style.top) - 5 + "px";
-}
-function moveRight() {
-    pm.style.left = parseInt(pm.style.left) + 5 + "px";
-}
-function moveDown() {
-    pm.style.top = parseInt(pm.style.top) + 5 + "px";
-}
 
 
 
 */
 
-var game_on;
+
+var game_on = false;
 document.addEventListener('keydown', keyPressed);
 
 function keyPressed(e) {
-    if (e.keyCode == 32){   // space
-        if(game_on){
+
+    document.getElementById("demo2").innerHTML = e.keyCode;
+
+
+    if(game_on){
+        if(e.keyCode == 32){     //pause
             game_on = false;
             pause();
         }
         else{
-            game_on = true;
-            start();
+            move();
         }
-    } 
+    }
+    else{
+        game_on = true;
+        start(); ss
+    }
 }
 
 function start(){
     document.getElementById("demo1").innerHTML = "GAME ON"
+    document.getElementById("startinfo").innerHTML = "[running]";
+    document.getElementById("demo2").style.display = "block";
 }
 
 
 function pause(){
     document.getElementById("demo1").innerHTML = "GAME PAUSED"
+    document.getElementById("startinfo").innerHTML = "press space to start the game";
+    document.getElementById("demo2").style.display = "none";
+
+}
+
+//  moving functions
+
+function move(e) {
+    // pm = document.getElementById("pacman");
+    pm = document.getElementById("pacman");
+
+
+    var key_code = e.which || e.keyCode;
+    switch (key_code) {
+        // case 37: moveLeft(pm); break;
+        // case 38: moveUp(pm); break;
+        // case 39: moveRight(pm); break;
+        // case 40: moveDown(pm); break;
+
+        case 37: pm.style.left = parseInt(pm.style.left) - 5 + "px"; break;
+        case 38: pm.style.top = parseInt(pm.style.top) - 5 + "px"; break;
+        case 39: pm.style.left = parseInt(pm.style.left) + 5 + "px"; break;
+        case 40: pm.style.top = parseInt(pm.style.top) + 5 + "px"; break;
+    }
+}
+//todo: capire ParseInt
+function moveLeft(pm) {
+    pm.style.left = parseInt(pm.style.left) - 5 + "px";
+}
+function moveUp(pm) {
+    pm.style.top = parseInt(pm.style.top) - 5 + "px";
+}
+function moveRight(pm) {
+    pm.style.left = parseInt(pm.style.left) + 5 + "px";
+}
+function moveDown(pm) {
+    pm.style.top = parseInt(pm.style.top) + 5 + "px";
 }
