15d30635824cfee7cc832787d987a87279a8c588
Filippo <pippodelministro@gmail.com>
2023-03-20 17:24:05 +0100

    removed rubbish

--- js/pacman-game.js
+++ js/pacman-game.js
@@ -1,40 +1,9 @@
-/*
-var level = 0;
-
-function start(level){
-    playground = document.getElementById("playground");
-    playground.style.cursor = "none";
-    pacmanInit()
-    game = new Game(level);
-}
-
-function Game(level){
-    document.addEventListener("keyup", this.key)
-}
-
-
-function pacmanInit() {
-    pm = document.getElementById("pacman");
-    pm.style.position = "relative";
-    pm.style.left = "0px";
-    pm.style.top = "0px";
-}
-
-
-
-
-*/
-
 
 var game_on = false;
-document.addEventListener('keydown', keyPressed);
+document.addEventListener('keyup', keyPressed);
 
 function keyPressed(e) {
-
-    
-    
     if(game_on == true){
-        document.getElementById("demo2").innerHTML = e.keyCode;
         if(e.keyCode == 32){     //pause
             pause();
         }
@@ -64,47 +33,25 @@ function pause(){
     game_on = false; 
 }
 
+
 //  moving functions
-
 function move(e) {
-    pm = document.getElementById("pacman");
-    // pm.style.position = "relative";
-    // pm.style.left = "0px";
-    // pm.style.top = "0px";
+    // let pm = document.getElementById("pacman");
+    document.getElementById("demo2").innerHTML = e.keyCode;
 
-
-    var key_code = e.which || e.keyCode;
-    switch (key_code) {
-        case 37: moveLeft(pm); break;
-        case 38: moveUp(pm); break;
-        case 39: moveRight(pm); break;
-        case 40: moveDown(pm); break;
-    
-            // case 37: pm.style.left = parseInt(pm.style.left) - 5 + "px"; break;
-            // case 38: pm.style.top = parseInt(pm.style.top) - 5 + "px"; break;
-            // case 39: pm.style.left = parseInt(pm.style.left) + 5 + "px"; break;
-            // case 40: pm.style.top = parseInt(pm.style.top) + 5 + "px"; break;
+    // var key_code = e.which || e.keyCode;
+    switch (e.keyCode) {
+        case 37: moveLeft(); break;
+        case 38: moveUp(); break;
+        case 39: moveRight(); break;
+        case 40: moveDown(); break;
     }
 }
-//todo: capire ParseInt
-function moveLeft(pm) {
-    pm.style.left = pm.style.left - 5 + "px";
 
-    // pm.style.left = parseInt(pm.style.left) - 5 + "px";
-}
-function moveUp(pm) {
-    pm.style.top = pm.style.left - 5 + "px";
 
-    // pm.style.top = parseInt(pm.style.top) - 5 + "px";
-}
-function moveRight(pm) {
-    pm.style.left = pm.style.left + 5 + "px";
-
-    // pm.style.left = parseInt(pm.style.left) + 5 + "px";
-}
-function moveDown(pm) {
-    pm.style.top = pm.style.left+ 5 + "px";
-
-    // pm.style.top = parseInt(pm.style.top) + 5 + "px";
+function moveLeft() {
+    document.getElementById("demo3").innerHTML = "moveLeft()";
+    pm = document.getElementById("pacman");
+    pm.style.left = parseInt(pm.style.left) - 5 + "px";
 }
 
