658995fb2b7142e15c260fe190c10369fe8e9fbe
Filippo <pippodelministro@gmail.com>
2023-03-31 19:00:07 +0200

    adding food and walls to map [n-w]

---
+++ js/pacman-map.js
@@ -0,0 +1,7 @@
+// const gridContainer = document.querySelector('.grid-container');
+
+// for (let i = 0; i < 15 * 15; i++) {
+//     const food = document.createElement('div');
+//     food.classList.add('food');
+//     gridContainer.appendChild(food);
+// }
