2847394481b36de2dc4e37c1548a532460eeafe6
Filippo <pippodelministro@gmail.com>
2023-03-31 19:14:45 +0200

    add food to map [w]

--- js/pacman-map.js
+++ js/pacman-map.js
@@ -1,7 +1,20 @@
-// const gridContainer = document.querySelector('.grid-container');
 
-// for (let i = 0; i < 15 * 15; i++) {
-//     const food = document.createElement('div');
-//     food.classList.add('food');
-//     gridContainer.appendChild(food);
-// }
+
+function addFood(){
+    const gridContainer = document.querySelector('.map');
+    for (let i = 0; i < 15 * 15; i++) {
+        const food = document.createElement('div');
+        food.classList.add('food');
+        gridContainer.appendChild(food);
+    }
+}
+
+function addWallsToCells(cellIndexes) {
+    for (let i = 0; i < 15 * 15; i++) {
+        if(i == cellIndexes[i]){
+        const wall = document.createElement('div');
+        wall.classList.add('wall');
+        gridContainer.appendChild(wall);
+        }
+    }
+}
