ba1d46f35f0758a43150fb4e7a669de2413e6e02
Tiresiah <pippodelministro@gmail.com>
2023-04-05 18:40:12 +0200

    section hide when click [w]

--- js/home.js
+++ js/home.js
@@ -25,14 +25,22 @@ function changeView(){
 
 
 function appear(sectionId) {
+    var section = document.getElementById(sectionId);
+    var isVisible = section.classList.contains('appear');
+  
     var sections = document.querySelectorAll('.menu-section');
-        sections.forEach(function(section) {
-            section.classList.remove('appear');
+    sections.forEach(function(section) {
+      section.classList.remove('appear');
     });
   
-    var section = document.getElementById(sectionId);
-    section.classList.add('appear');
-
-    const div = document.getElementById("main-section");
-    div.style.transform = 'translateX(350px)';
-}
+    if (!isVisible) {
+      section.classList.add('appear');
+      const div = document.getElementById("main-section");
+      div.style.transform = 'translateX(350px)';
+    } else {
+      const div = document.getElementById("main-section");
+      setTimeout(1);
+      div.style.transform = 'translateX(0)';
+    }
+  }
+  
